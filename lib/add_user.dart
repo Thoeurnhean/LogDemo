6c4232eee4950e7c63531f0e356d08648a88114e
KokoJr <51535452+Thoeurnhean@users.noreply.github.com>
2021-03-06 12:33:31 +0700

    at cafe

--- lib/add_user.dart
+++ lib/add_user.dart
@@ -1,8 +1,11 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:percent_indicator/percent_indicator.dart';
+import 'package:rxdart/subjects.dart';
 
 class AddUser extends StatefulWidget {
   final String docId;
@@ -20,6 +23,13 @@ class _AddUserState extends State<AddUser> {
   bool isValidate = false;
   CollectionReference colRef = FirebaseFirestore.instance.collection('user');
   GlobalKey<ScaffoldState> _scaffoldState = GlobalKey<ScaffoldState>();
+  double percentUpload = 0.0;
+  var percentUploadSubject = BehaviorSubject<double>();
+  @override
+  dispose() {
+    super.dispose();
+    percentUploadSubject.close();
+  }
 
   _addDoc() {
     Map<String, dynamic> doc = {
@@ -43,6 +53,22 @@ class _AddUserState extends State<AddUser> {
 
   File _image;
   final picker = ImagePicker();
+  testingPercentUpload() {
+    var timer;
+   timer = Timer.periodic(Duration(milliseconds: 70), (timer) {
+     double maxkb = 5000.0;
+        if(timer.tick <= 100) {
+          // percentUpload += 0.2;
+          percentUpload += double.parse(((maxkb/100)/maxkb).toStringAsFixed(2));
+          percentUploadSubject.add(percentUpload);
+          print('${percentUpload}%');
+        }else{
+          percentUpload = 0.0;
+          percentUploadSubject.add(0.0);
+          timer.cancel();
+        }
+     });
+  }
 
   Future getImage(ImageSource source) async {
     final pickedFile = await picker.getImage(source: source);
@@ -50,7 +76,9 @@ class _AddUserState extends State<AddUser> {
     setState(() {
       if (pickedFile != null) {
         //create file where the photo path is stored
+        percentUpload  =0;
         _image = File(pickedFile.path);
+        testingPercentUpload();
       } else {
         print('No image selected.');
       }
@@ -102,20 +130,32 @@ class _AddUserState extends State<AddUser> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                  Container(
+                Container(
                     height: 150,
                     width: 150,
                     decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(8)),
-                      color: Colors.blue,
-                      image: _image == null ? null : DecorationImage(
-                        fit: BoxFit.cover,
-                        // colorFilter: ColorFilter.mode(Colors.red, BlendMode.color),
-                        image: FileImage(_image)
-                      )
-                    ),
-
-                  )
+                        borderRadius: BorderRadius.all(Radius.circular(8)),
+                        color: Colors.blue,
+                        image: _image == null
+                            ? null
+                            : DecorationImage(
+                                fit: BoxFit.cover,
+                                // colorFilter: ColorFilter.mode(Colors.red, BlendMode.color),
+                                image: FileImage(_image))),
+                    child: StreamBuilder(
+                      initialData: 0.0,
+                      stream: percentUploadSubject.stream,
+                      builder: (BuildContext context,
+                          AsyncSnapshot<dynamic> snapshot) {
+                        return snapshot.data == 0 ? Container() : new CircularPercentIndicator(
+                          radius: 100.0,
+                          lineWidth: 10.0,
+                          percent: snapshot.data,
+                          backgroundColor: Colors.grey,
+                          progressColor: Colors.blue,
+                        );
+                      },
+                    ))
               ],
             ),
             SizedBox(
