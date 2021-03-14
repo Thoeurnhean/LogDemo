7572a39fd4a28c6e8a1edc4569cfe3ed029854e8
iamfourh2e <iamfourh2e@gmail.com>
2021-03-14 11:41:18 +0700

    update upload

--- lib/add_user.dart
+++ lib/add_user.dart
@@ -1,7 +1,8 @@
 import 'dart:async';
 import 'dart:io';
-
+import 'package:path/path.dart' as p;
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:percent_indicator/percent_indicator.dart';
@@ -27,6 +28,7 @@ class _AddUserState extends State<AddUser> {
   GlobalKey<ScaffoldState> _scaffoldState = GlobalKey<ScaffoldState>();
   double percentUpload = 0.0;
   var percentUploadSubject = BehaviorSubject<double>();
+
   @override
   dispose() {
     super.dispose();
@@ -55,21 +57,10 @@ class _AddUserState extends State<AddUser> {
 
   File _image;
   final picker = ImagePicker();
-  testingPercentUpload() {
-    var timer;
-   timer = Timer.periodic(Duration(milliseconds: 70), (timer) {
-     double maxkb = 5000.0;
-        if(timer.tick <= 100) {
-          // percentUpload += 0.2;
-          percentUpload += double.parse(((maxkb/100)/maxkb).toStringAsFixed(2));
-          percentUploadSubject.add(percentUpload);
-          print('${percentUpload}%');
-        }else{
-          percentUpload = 0.0;
-          percentUploadSubject.add(0.0);
-          timer.cancel();
-        }
-     });
+
+  testingPercentUpload(num maxkb, num transferedByte) {
+    percentUpload = (transferedByte / maxkb);
+    percentUploadSubject.add(percentUpload);
   }
 
   Future getImage(ImageSource source) async {
@@ -78,9 +69,27 @@ class _AddUserState extends State<AddUser> {
     setState(() {
       if (pickedFile != null) {
         //create file where the photo path is stored
-        percentUpload  =0;
+        percentUpload = 0;
         _image = File(pickedFile.path);
-        testingPercentUpload();
+        FirebaseStorage.instance
+            .ref(
+                'images/${p.basename(_image.path).replaceAll('image_picker', '')}')
+            .putFile(_image)
+            .snapshotEvents
+            .listen((event) {
+          if (event.state == TaskState.running) {
+            testingPercentUpload(event.totalBytes, event.bytesTransferred);
+          }
+          if (event.state == TaskState.error) {
+            print("Can not upload image");
+          }
+
+          if (event.state == TaskState.success) {
+            //retrieve uploaded image url
+            //TODO: take this url to store in collection
+            event.ref.getDownloadURL().then((value) => print(value));
+          }
+        });
       } else {
         print('No image selected.');
       }
@@ -104,10 +113,10 @@ class _AddUserState extends State<AddUser> {
   Widget build(BuildContext context) {
     return Scaffold(
       key: _scaffoldState,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => getImage(ImageSource.camera),
-        child: Icon(Icons.camera_alt_outlined),
-      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () => getImage(ImageSource.camera),
+      //   child: Icon(Icons.camera_alt_outlined),
+      // ),
       appBar: AppBar(
         title: Text("Add user"),
       ),
@@ -132,36 +141,84 @@ class _AddUserState extends State<AddUser> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Container(
-                    height: 150,
-                    width: 150,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(8)),
-                        color: Colors.blue,
-                        image: _image == null
-                            ? null
-                            : DecorationImage(
-                                fit: BoxFit.cover,
-                                // colorFilter: ColorFilter.mode(Colors.red, BlendMode.color),
-                                image: FileImage(_image))),
-                    child: StreamBuilder(
-                      initialData: 0.0,
-                      stream: percentUploadSubject.stream,
-                      builder: (BuildContext context,
-                          AsyncSnapshot<dynamic> snapshot) {
-                        return snapshot.data == 0 ? Container() : new CircularPercentIndicator(
-                          radius: 100.0,
-                          lineWidth: 10.0,
-                          percent: snapshot.data,
-                          backgroundColor: Colors.grey,
-                          progressColor: Colors.blue,
-                        );
-                      },
-                    ))
+                InkWell(
+                  onTap: () {
+                    showModalBottomSheet(
+                        context: context,
+                        builder: (_) {
+                          return Container(
+                            height: 100,
+                            child: Column(
+                              children: [
+                                Expanded(
+                                    child: InkWell(
+                                  onTap: () {
+                                    getImage(ImageSource.gallery);
+                                    Navigator.pop(context);
+                                  },
+                                  child: Row(
+                                    children: [
+                                      SizedBox(width: 20),
+                                      Icon(Icons.photo),
+                                      SizedBox(width: 20),
+                                      Text('Gallery')
+                                    ],
+                                  ),
+                                )),
+                                Expanded(
+                                    child: InkWell(
+                                  onTap: () {
+                                    getImage(ImageSource.camera);
+                                    Navigator.pop(context);
+                                  },
+                                  child: Row(
+                                    children: [
+                                      SizedBox(width: 20),
+                                      Icon(Icons.camera_alt),
+                                      SizedBox(width: 20),
+                                      Text('Camera')
+                                    ],
+                                  ),
+                                ))
+                              ],
+                            ),
+                          );
+                        });
+                  },
+                  child: Container(
+                      height: 150,
+                      width: 150,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(8)),
+                          color: Colors.white,
+                          boxShadow: [],
+                          image: _image == null
+                              ? null
+                              : DecorationImage(
+                                  fit: BoxFit.cover,
+                                  // colorFilter: ColorFilter.mode(Colors.red, BlendMode.color),
+                                  image: FileImage(_image))),
+                      child: StreamBuilder(
+                        initialData: 0.0,
+                        stream: percentUploadSubject.stream,
+                        builder: (BuildContext context,
+                            AsyncSnapshot<dynamic> snapshot) {
+                          return snapshot.data == 0
+                              ? Container()
+                              : new CircularPercentIndicator(
+                                  radius: 100.0,
+                                  lineWidth: 10.0,
+                                  percent: snapshot.data,
+                                  backgroundColor: Colors.grey,
+                                  progressColor: Colors.blue,
+                                );
+                        },
+                      )),
+                )
               ],
             ),
             SizedBox(
-              height: 10,
+              height: 50,
             ),
             TextFormField(
               validator: (val) {
